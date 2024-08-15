50ea673662329157c1c545339137271633c4cdb9
Samia Nasreen <samia.nasreen@brainstation-23.com>
2024-08-16 02:06:41 +0600

    testing assets in public directory

--- src/data/sliderImages.js
+++ src/data/sliderImages.js
@@ -1,7 +1,7 @@
 export const sliderImages = [
-  "/src/assets/test.jpg",
-  "/src/assets/iphone6.jpeg",
-  "/src/assets/iphone4.jpeg",
-  "/src/assets/iphone5.webp",
-  "/src/assets/iphone7.webp",
+  "/assets/test.jpg",
+  "/assets/iphone6.jpeg",
+  "/assets/iphone4.jpeg",
+  "/assets/iphone5.webp",
+  "/assets/iphone7.webp",
 ];
