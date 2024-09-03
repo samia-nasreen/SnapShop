f3ed3865b999792a7c514da4c7fe3118422fa736
Samia Nasreen <samia.nasreen@brainstation-23.com>
2024-09-03 17:40:23 +0600

    minor css fixes

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -6,7 +6,7 @@ import Breadcrumb from "../../components/UI/Breadcrumb";
 
 const AboutPage = () => {
   return (
-    <div className="px-4 sm:px-8 sm:px-8 md:px-16 lg:px-28">
+    <div className="px-4 sm:px-8 md:px-16 lg:px-28">
       <Breadcrumb parts={["Home", "About"]} className="ml-4" />
       <OurStory />
       <Stats />
